0f4c4d7eb4f01e07c48d9df3bd51b2eafe145bc3
kelicom <735579768@qq.com>
2015-10-17 16:16:33 +0800

    完善简化编辑器

--- Public/Static/ueditor/umeditor.config.js
+++ Public/Static/ueditor/umeditor.config.js
@@ -34,12 +34,11 @@
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         ,toolbars: [[
-            'fullscreen', 'source', '|', 'undo', 'redo', '|',
-            'bold', 'italic', 'underline',  'strikethrough', 'removeformat', '|', 'forecolor', 'backcolor', 'selectall','|',
-             'paragraph', 'fontfamily', 'fontsize', '|',
-            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|',
-            'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
-            'simpleupload', 'insertimage', 'emotion',  'music', 'attachment', 'map', '|'
+            'fullscreen', 'source', '|', 'fontsize',
+            'bold', 'italic', 'underline',  'strikethrough', 'removeformat', '|', 'forecolor', 'backcolor',  '|',
+            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify',
+            'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+            'simpleupload', 'emotion'
         ]]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
