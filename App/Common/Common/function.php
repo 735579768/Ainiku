d29f914197ea21dac896ab06561b517f2f9b7a7b
kelicom <735579768@qq.com>
2015-11-13 15:08:11 +0800

    添加http_post函数

--- App/Common/Common/function.php
+++ App/Common/Common/function.php
@@ -834,3 +834,23 @@ function file_ismod($filepath){
 	$config =   api('Config/lists');
 	F('DB_CONFIG_DATA',$config);	 
 	 }
+/**
+ * 发送post请求
+ * @param string $url 请求地址
+ * @param array $post_data post键值对数据
+ * @return string
+ */
+function http_post($url, $post_data) {
+    $postdata = http_build_query($post_data);
+    $options = array(
+        'http' =>array(
+            'method' =>'POST',
+            'header' =>'Content-type:application/x-www-form-urlencoded',
+            'content' =>$postdata,
+            'timeout' =>15 * 60 // 超时时间（单位:s）
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    return $result;
+}
