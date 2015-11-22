5b9a010a0eb7489082b07046c3220189c2c784b6
kelicom <735579768@qq.com>
2015-11-22 22:22:33 +0800

    前台添加一个关联模型

---
+++ App/Home/Model/GoodsModel.class.php
@@ -0,0 +1,25 @@
+<?php
+// | Author: 枫叶 <735579768@qq.com> <http://www.zhaokeli.com>
+// +----------------------------------------------------------------------
+namespace Home\Model;
+use Think\Model\RelationModel;
+if(!defined("ACCESS_ROOT"))die("Invalid access");
+
+/**
+ * 用户模型
+ * @author 寞枫 <735579768@qq.com>
+ */
+
+class GoodsModel extends RelationModel{
+	 protected $_link = array(
+			'GoodsAttribute' => array(
+				'mapping_type'  => self::HAS_MANY,
+				'class_name'    => 'GoodsAttribute',
+				'mapping_name'  => 'Goodsattribute',
+				'parent_key'	=>'goods_id',
+				'foreign_key'   => 'goods_id',
+				'mapping_order' => 'goods_attribute_id desc',
+				'mapping_fields'=>'name,value',
+			),
+		);
+}
