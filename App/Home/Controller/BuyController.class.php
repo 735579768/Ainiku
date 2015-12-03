71f0309ac40bcdef7be12ad7837cf1efa60683aa
kelicom <735579768@qq.com>
2015-12-03 17:25:34 +0800

    添加收货地址页面

--- App/Home/Controller/BuyController.class.php
+++ App/Home/Controller/BuyController.class.php
@@ -3,5 +3,11 @@ namespace Home\Controller;
 use Think\Controller;
 if(!defined("ACCESS_ROOT"))die("Invalid access");
 class BuyController extends LoginController {
- 
+ public function checkout(){
+ 	$this->display('checkout');
+ }
+ public function addaddress(){
+ 	$this->success($this->fetch('addaddress'));
+ 	die();
+ }
 }
