758212ef3c366c68402ae6b2dd4ca9e38e6db044
kelicom <735579768@qq.com>
2015-12-04 00:07:03 +0800

    完善更新地址功能

--- App/Home/Controller/BuyController.class.php
+++ App/Home/Controller/BuyController.class.php
@@ -4,10 +4,82 @@ use Think\Controller;
 if(!defined("ACCESS_ROOT"))die("Invalid access");
 class BuyController extends LoginController {
  public function checkout(){
+ 	//取地址列表
+ 	//$map['uid']=UID;
+ 	$list=M('ConsigneeAddress')->where($map)->select();
+ 	$this->assign('addresslist',$list);
  	$this->display('checkout');
  }
  public function addaddress(){
- 	$this->success($this->fetch('addaddress'));
- 	die();
+ 	if(IS_POST){
+ 		$model=M('ConsigneeAddress');
+ 		if($model->create()){
+ 			$data['address_id']=$model->consignee_address_id;
+ 			$data['name']=$model->consignee_name;
+ 			$data['mobile']=$model->consignee_mobile;
+ 			$data['diqu']=getRegion($model->consignee_diqu).'<br>'.$model->consignee_detail;
+ 			if(empty($model->consignee_address_id)){
+	 			$model->create_time=NEW_TIME;
+	 			$model->update_time=NEW_TIME;
+	 			$result=$model->add();
+	 			if($result>0){
+	 				$this->ajaxreturn(array(
+	 							status=>1,
+	 							action=>'add',
+	 							info=>'添加成功',
+	 							data=>$data,
+	 							));
+	 			}else{
+	 				$this->error('添加失败');
+	 			}
+ 			}else{
+	 			$model->update_time=NEW_TIME;
+	 			$result=$model->save();
+	 			if($result>0){
+	 				$this->ajaxreturn(array(
+	 							status=>1,
+	 							action=>'edit',
+	 							info=>'修改成功',
+	 							data=>$data,
+	 							));
+	 			}else{
+	 				$this->error('没有更改');
+	 			} 				
+ 			}
+
+ 		}else{
+ 			$this->error($model->geterror());
+ 		}
+ 	}else{
+ 		$this->success($this->fetch('addaddress'));
+ 	}
+ }
+ public function editaddress(){
+ 	$consignee_address_id=I('consignee_address_id');
+ 	$action=I('action');
+ 	empty($consignee_address_id)&&$this->error('非法请求');
+ 	if($action=='edit'){
+ 	 	//$map['uid']=UID;
+	 	$map['consignee_address_id']=$consignee_address_id;
+	 	$info=M('ConsigneeAddress')->where($map)->find();
+	 	$this->assign('info',$info);
+	 	$this->success($this->fetch('addaddress'));		
+ 	}
+ 	if(IS_POST){
+		$model=M('ConsigneeAddress');
+		if($model->create()){
+			//$model->create_time=NEW_TIME;
+ 			$model->update_time=NEW_TIME;
+ 			$result=$model->save();
+ 			if($result>0){
+ 				$this->success('修改成功');
+ 			}else{
+ 				$this->error('修改失败');
+ 			}
+ 		}else{
+ 			$this->error($model->geterror());
+ 		}
+ 	}
+
  }
 }
