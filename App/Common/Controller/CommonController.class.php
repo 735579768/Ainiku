7ddfef2ad64e521489f89d91ff6edd95fdd9c1fe
kelicom <735579768@qq.com>
2016-08-03 10:09:37 +0800

    添加空控制器404和异常判断

--- App/Common/Controller/CommonController.class.php
+++ App/Common/Controller/CommonController.class.php
@@ -43,9 +43,11 @@ dump($assets);*/
 			header('HTTP/1.1 404 Not Found');
 			header("status: 404 Not Found");
 			$this->display('Public:404');
+		} else {
+			//调试状态下抛出异常
+			E(L('_ERROR_ACTION_') . ':' . ACTION_NAME);
 		}
 
-		die();
 	}
 	/**
 	 * 前台台控制器初始化
