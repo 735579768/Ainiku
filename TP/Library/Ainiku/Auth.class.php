fd195fb6718ff36b6da4678363d30844b0ca062b
kelicom <735579768@qq.com>
2016-08-26 10:51:42 +0800

    修复权限控制

--- TP/Library/Ainiku/Auth.class.php
+++ TP/Library/Ainiku/Auth.class.php
@@ -55,62 +55,53 @@ class Auth {
 	}
 	//把没有权限的链接替换掉
 	function replaceurl($str = null) {
+		//return $str;
 		if (IS_ADMIN) {
 			return $str;
 		} else {
 			if (!empty($this->noaccessnodelist)) {
+				$reg_arr = [];
 				//正则替换链接
 				$are = '([^<|^>]*?)';
 				foreach ($this->noaccessnodelist as $val) {
 					$url = trim($val['name']);
 					if (!empty($url)) {
 						$pattern = '/';
+						//把不带/的当成整个模块都不让访问
 						if (strpos($url, '/') === false) {
 							$url1 = U("{$url}/index");
 							$url  = preg_replace('/\&a\=.*/', '', $url1);
 							$url  = preg_quote($url);
 							$url  = substr($url, 1);
-							$pattern .= '<[tag]>[tag]<[tag]' . $url . '[tag]>[tag]<[tag]>[tag]<[tag]>';
+							$pattern .= '<[tag]' . $url . '[tag]>[tag]<[tag]>';
 							$pattern .= '/';
-							$pattern = str_replace('[tag]', '([^<|^>]*?)', $pattern);
-							$str     = preg_replace($pattern, '', $str);
+							$pattern   = str_replace('[tag]', '([^<|^>]*?)', $pattern);
+							$reg_arr[] = $pattern;
+							// $str       = preg_replace($pattern, '', $str);
 						} else {
 							$url = U($url);
 							$url = str_replace(array('.' . C('URL_HTML_SUFFIX')), array(''), $url);
 							$url = preg_quote($url);
 							$url = str_replace('/', '\/', $url);
-//把链接按钮(带有btn的操作)替换掉
+							//把链接按钮(带有btn的操作)替换掉
 							if ($val['is_all'] == 1) {
 								$pattern .= '<[tag]' . $url . '[tag]>[tag]<[tag]>';
 								$pattern .= '|<([^<|^>|^\/]*?)>[tag]<[tag]' . $url . '[tag]>[tag]<\/[tag]>[tag]<\/[tag]>';
 							} else {
-//$url=preg_replace('/(\?)|(\=)|(\/)|(\.)/i','\\\$1$2$3$4',$url);
+								//$url=preg_replace('/(\?)|(\=)|(\/)|(\.)/i','\\\$1$2$3$4',$url);
 								$pattern .= '<[tag]' . $url . '[tag]>[tag]<[tag]>';
 								$pattern .= '|<([^<|^>|^\/]*?)>[tag]<[tag]' . $url . '[tag]>[tag]<\/[tag]>[tag]<\/[tag]>';
 							}
 							$pattern .= '/i';
-							$pattern = str_replace('[tag]', '([^<|^>]*?)', $pattern);
-							//trace($pattern);
-							$str = preg_replace($pattern, '', $str);
+							$pattern   = str_replace('[tag]', '([^<|^>]*?)', $pattern);
+							$reg_arr[] = $pattern;
+							// $str       = preg_replace($pattern, '', $str);
 						}
 						//echo $pattern.'--<br>';
 
 					}
 				}
-				//移除后台左边空的菜单项目
-				$matchs = array();
-				$mat    = preg_match_all('/<dl[^<|^>]*?class\=\"menu\">[\s\S]*?<\/dl>/iu', $str, $matchs);
-				foreach ($matchs[0] as $val) {
-					if (strpos($val, 'mitem') === false) {
-						$str = str_replace($val, '', $str);
-					}
-				}
-				//判断有没有按钮替换换操作标题
-				$pattern = '/<td([\s\S]*?)>([\s\S]*?)<a([\s\S]*?)btn([\s\S]*?)>([\s\S]*?)<\/a>([\s\S]*?)<\/td>/i';
-				$resu    = preg_match($pattern, $str, $matchs);
-				if ($resu === 0) {
-					$str = preg_replace('/<th(.*?)>(.*?)REPLACE_ACTION(.*?)<\/th>/', '', $str);
-				}
+				$str = preg_replace($reg_arr, '', $str);
 			}
 			return $str;
 		}
