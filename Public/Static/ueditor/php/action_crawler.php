00f80fbf32e485ae6622bf6912a693090106965c
zhaokeli <735579768@qq.com>
2016-10-08 10:27:48 +0800

    update

--- Public/Static/ueditor/php/action_crawler.php
+++ Public/Static/ueditor/php/action_crawler.php
@@ -1,4 +1,5 @@
 <?php
+defined("ACCESS_ROOT") || die("Invalid access");
 /**
  * 抓取远程图片
  * User: Jinqn
@@ -6,39 +7,39 @@
  * Time: 下午19:18
  */
 set_time_limit(0);
-include("Uploader.class.php");
+include "Uploader.class.php";
 
 /* 上传配置 */
 $config = array(
-    "pathFormat" => $CONFIG['catcherPathFormat'],
-    "maxSize" => $CONFIG['catcherMaxSize'],
-    "allowFiles" => $CONFIG['catcherAllowFiles'],
-    "oriName" => "remote.png"
+	"pathFormat" => $CONFIG['catcherPathFormat'],
+	"maxSize"    => $CONFIG['catcherMaxSize'],
+	"allowFiles" => $CONFIG['catcherAllowFiles'],
+	"oriName"    => "remote.png",
 );
 $fieldName = $CONFIG['catcherFieldName'];
 
 /* 抓取远程图片 */
 $list = array();
 if (isset($_POST[$fieldName])) {
-    $source = $_POST[$fieldName];
+	$source = $_POST[$fieldName];
 } else {
-    $source = $_GET[$fieldName];
+	$source = $_GET[$fieldName];
 }
 foreach ($source as $imgUrl) {
-    $item = new Uploader($imgUrl, $config, "remote");
-    $info = $item->getFileInfo();
-    array_push($list, array(
-        "state" => $info["state"],
-        "url" => $info["url"],
-        "size" => $info["size"],
-        "title" => htmlspecialchars($info["title"]),
-        "original" => htmlspecialchars($info["original"]),
-        "source" => htmlspecialchars($imgUrl)
-    ));
+	$item = new Uploader($imgUrl, $config, "remote");
+	$info = $item->getFileInfo();
+	array_push($list, array(
+		"state"    => $info["state"],
+		"url"      => $info["url"],
+		"size"     => $info["size"],
+		"title"    => htmlspecialchars($info["title"]),
+		"original" => htmlspecialchars($info["original"]),
+		"source"   => htmlspecialchars($imgUrl),
+	));
 }
 
 /* 返回抓取数据 */
 return json_encode(array(
-    'state'=> count($list) ? 'SUCCESS':'ERROR',
-    'list'=> $list
+	'state' => count($list) ? 'SUCCESS' : 'ERROR',
+	'list'  => $list,
 ));
